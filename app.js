771212a1a514d88f48fabccd8e25a7441c8a39c2
fsrocha-dev <contato.frankrocha@gmail.com>
2020-05-19 20:12:07 -0300

    criando base do servidor

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const env = Deno.env.toObject();
+const PORT = env.PORT || 4000;
+const HOST = env.HOST || '127.0.0.1';
+
+const router = new Router();
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Servidor na porta ${PORT}...`);
+
+await app.listen(`${HOST}:${PORT}`);
